9ba765efe60dcfe2bd69042c36fbe414f0ea0cc5
rektion <redutilos0@gmail.com>
2019-11-16 20:11:18 +0100

    Board singleton Board observer player

---
+++ Monopoly/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    public class Player
+    {
+        static Board board;
+        private string name;
+        public ushort position;
+        private uint money;
+
+        public void teleport(ushort position)
+        {
+            this.position = (ushort)(position % 40);
+            board.PositionUpdate(this);
+        }
+    }
+}
